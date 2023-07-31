14a330027dd22058c822c697dc8e36715f219c8b
Sardor <sardorsoftwaredeveloper@gmail.com>
2023-07-31 20:20:54 +0200

    other pages added and modified

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path, include
-from .views import HomePageView, AboutPageView, ContactCreateView, ProcessView
+from .views import HomePageView, AboutPageView, ContactCreateView, ProcessView, OurServicesView, StreichenView, \
+    FliesenVerlegenView, SpachtelnView, TuerEinrichtenView, TapezierenView, MoebelAufbauenView, LaminatParkettView, GipskartonplatteView, \
+    ImpressumView, DataProtectionView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about', AboutPageView.as_view(), name='about'),
     path('contact', ContactCreateView.as_view(), name='contact'),
     path('process', ProcessView.as_view(), name='process'),
+    path('our-services', OurServicesView.as_view(), name='our-services'),
+    path('streichen', StreichenView.as_view(), name='streichen'),
+    path('fliesen-verlegen', FliesenVerlegenView.as_view(), name='fliesen-verlegen'),
+    path('spachteln', SpachtelnView.as_view(), name='spachteln'),
+    path('tuer-einrichten', TuerEinrichtenView.as_view(), name='tuer-einrichten'),
+    path('tapezieren', TapezierenView.as_view(), name='tapezieren'),
+    path('moebel-aufbauen', MoebelAufbauenView.as_view(), name='moebel-aufbauen'),
+    path('laminat-parkett', LaminatParkettView.as_view(), name='laminat-parkett'),
+    path('gipskartonplatte', GipskartonplatteView.as_view(), name='gipskartonplatte'),
+    path('impressum', ImpressumView.as_view(), name='impressum'),
+    path('data-protection', DataProtectionView.as_view(), name='data-protection'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -14,3 +14,36 @@ class ContactCreateView(TemplateView):
 
 class ProcessView(TemplateView):
     template_name = 'process.html'
+
+class OurServicesView(TemplateView):
+    template_name = 'our-services.html'
+
+class StreichenView(TemplateView):
+    template_name = 'streichen.html'
+
+class FliesenVerlegenView(TemplateView):
+    template_name = 'fliesen-verlegen.html'
+
+class SpachtelnView(TemplateView):
+    template_name = 'spachteln.html'
+
+class TuerEinrichtenView(TemplateView):
+    template_name = 'tuer-einrichten.html'
+
+class TapezierenView(TemplateView):
+    template_name = 'tapezieren.html'
+
+class MoebelAufbauenView(TemplateView):
+    template_name = 'moebel-aufbauen.html'
+
+class LaminatParkettView(TemplateView):
+    template_name = 'laminat-parkett.html'
+
+class GipskartonplatteView(TemplateView):
+    template_name = 'gipskartonplatte.html'
+
+class ImpressumView(TemplateView):
+    template_name = 'impressum.html'
+
+class DataProtectionView(TemplateView):
+    template_name = 'data-protection.html'
