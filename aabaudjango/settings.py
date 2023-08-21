9cb29d2501fb9b9160d3f4d4ae33fc770f1bc998
Sardor <sardorsoftwaredeveloper@gmail.com>
2023-08-21 22:54:35 +0200

    added allowed hosts

--- aabaudjango/settings.py
+++ aabaudjango/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = 'z(lg7x6j28(r*ii)r3z5d+%00dg36g4yz&0)hg7_!0y97=aaj5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['samsobirov.pythonanywhere.com', '127.0.0.1']
-
+ALLOWED_HOSTS = ['samsobirov.pythonanywhere.com', '194.163.167.46', '127.0.0.1', 'localhost']
 
 # Application definition
 
