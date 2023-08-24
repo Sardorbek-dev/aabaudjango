ab8f090a059eb721adc0aa6a4cbb3b2e1162da63
Sardor <sardorsoftwaredeveloper@gmail.com>
2023-08-24 22:56:40 +0200

    set debug to false

--- aabaudjango/settings.py
+++ aabaudjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'z(lg7x6j28(r*ii)r3z5d+%00dg36g4yz&0)hg7_!0y97=aaj5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.aplusabau.de', 'aplusabau.de', '194.163.167.46']
 
 # Application definition
